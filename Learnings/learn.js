4032f736dec57a71cfb7d4b0707dbf3c2058eb30
Damola <odamola@mit.edu>
2024-08-21 11:56:43 +0100

    learn commit

--- Learnings/learn.js
+++ Learnings/learn.js
@@ -1,18 +1,20 @@
+console.log(window.indexedDB);
+
 // console.error("this is an erorr");
 // console.warn("this is a warning")
 
 
-const person = {
-    firstName: "John",
-    lastName:"Seamsons",
-    age: 30,
-    address: {
-        street: "123 Main St",
-        city: "Anytown",
-        state: "CA",
-        zip: "12345"
-    }
-}
+// const person = {
+//     firstName: "John",
+//     lastName:"Seamsons",
+//     age: 30,
+//     address: {
+//         street: "123 Main St",
+//         city: "Anytown",
+//         state: "CA",
+//         zip: "12345"
+//     }
+// }
 
-const { firstName,  lastName, age, address: {street, city} } = person;
+// const { firstName,  lastName, age, address: {street, city} } = person;
 
